b594ad4d2e259de631c0dc9833ae88c69c5b2e0c
DemmyJay <demmyjyd@gmail.com>
2025-08-11 23:20:28 +0100

    test responsiveness with lorem

--- scripts/header.js
+++ scripts/header.js
@@ -1,7 +1,9 @@
 const hamburger = document.getElementById('hamburger');
 const nav = document.getElementById('nav-links');
+const body = document.querySelector('body');
 
 hamburger.addEventListener('click', () => {
     nav.classList.toggle('show');
+    body.style.overflow = "hidden";
 }
 )
