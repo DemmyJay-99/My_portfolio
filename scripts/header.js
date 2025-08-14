799009a98547c50871213b3fadc5ff8587a687ee
DemmyJay <demmyjyd@gmail.com>
2025-08-14 20:09:42 +0100

    Add sections

--- scripts/header.js
+++ scripts/header.js
@@ -1,8 +1,22 @@
 const hamburger = document.getElementById('hamburger');
 const nav = document.getElementById('nav-links');
 const body = document.querySelector('body');
+let lastScrollTop = 0;
 
 hamburger.addEventListener('click', () => {
     const isOpen = nav.classList.toggle('show');
     body.style.overflowY = isOpen ? 'hidden' : 'auto';
 });
+
+// handleScroll() {
+//         const scrollTop = window.pageYOffset || document.documentElement.scrollTop;
+        
+//         // Add/remove scrolled class
+//         if (scrollTop > 50) {
+//             nav.classList.add('scrolled');
+//         } else {
+//             nav.classList.remove('scrolled');
+//         }
+        
+//         lastScrollTop = scrollTop;
+//     }
