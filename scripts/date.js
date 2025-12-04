ab3c1998fe1da2cc6e4c190409296a2bc070ebaf
DemmyJay <demmyjyd@gmail.com>
2025-12-04 17:29:25 +0100

    Add footer section

--- scripts/date.js
+++ scripts/date.js
@@ -1,3 +1,8 @@
 const date = new Date();
 const year = date.getFullYear()
+const span = document.getElementById('date');
+
+(()=>{
+    span.innerHTML = `&copy;` + ` ${year}` + ` All rights reserved`
+})()
 console.log(year)
