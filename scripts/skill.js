b4c1448d746aa0368614a6509fca92d15a4f723f
DemmyJay <demmyjyd@gmail.com>
2025-12-05 03:58:03 +0100

    Fix responsiveness issues on smaller screens on skills and footer sections

--- scripts/skill.js
+++ scripts/skill.js
@@ -2,7 +2,8 @@ const categoryDivs = document.querySelectorAll('.skill-category');
 
 categoryDivs.forEach((c)=>{
     const cards = c.querySelectorAll('.skill-card');
-    const icon = c.querySelector('.fa-angle-right')
+    const icon = c.querySelector('.skill-icon');
+    const angleIcon = c.querySelector(".fa-angle-right")
     c.onclick = ()=>{
     cards.forEach((card )=>{
         const isOpen = card.classList.toggle("show")
@@ -10,5 +11,6 @@ categoryDivs.forEach((c)=>{
            card.style.animationName = isOpen ?  "fadein" : "fadeout";
     })
     icon.classList.toggle('active')
+    angleIcon.classList.toggle('active')
 }
 })
